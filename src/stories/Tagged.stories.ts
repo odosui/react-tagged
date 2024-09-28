f2121e28abb449f3db3470144baefe0d25a9872a
Ianis Triandafilov <janis.sci@gmail.com>
2024-09-28 18:50:22 +0200

    use storybook

---
+++ src/stories/Tagged.stories.ts
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Tagged } from '../Tagged'
+import { countries, states } from './helpers'
+
+const meta = {
+  title: 'Example/Tagged',
+  component: Tagged,
+  parameters: {
+    // layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    initialTags: {
+      description: 'Initial list of tags',
+      defaultValue: [],
+    },
+  },
+  // args: { initialTags: [] },
+} satisfies Meta<typeof Tagged>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SelectState: Story = {
+  name: 'Select US States',
+  args: {
+    initialTags: ['Nebraska', 'Nevada'],
+    suggestions: states,
+  },
+}
+
+export const CustomTagNotAllowed: Story = {
+  name: 'Custom tags not allowed',
+  args: {
+    initialTags: ['Denmark'],
+    suggestions: countries,
+    allowCustom: false,
+    reverse: true,
+  },
+}
+
+export const CustomPlaceholder: Story = {
+  args: {
+    initialTags: ['Denmark'],
+    suggestions: countries,
+    inputPlaceholder: 'Press enter',
+  },
+}
+
+export const SuggestionTreshold: Story = {
+  args: {
+    initialTags: ['Denmark'],
+    suggestions: countries,
+    suggestionsThreshold: 2,
+  },
+}
